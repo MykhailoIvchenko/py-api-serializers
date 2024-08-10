73853ff6b2efe2ad7fa7757e81daf334a761fd8f
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-08-10 15:41:35 +0300

    Create basic routes

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,15 @@
-# write urls here
+from django.urls import path, include
+from rest_framework import routers
+
+app_name = "cinema"
+
+router = routers.DefaultRouter()
+router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+router.register("cinema_halls", CinemaHallViewSet)
+router.register("movie", MovieViewSet)
+router.register("movie_session", MovieSessionViewSet)
+
+urlpatterns = [
+    path("", include("cinema.urls")),
+]
